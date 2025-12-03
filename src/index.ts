a882902bab4e5a5e5d57113797c79b27094cfe31
Matt Carter <mattuk@live.co.uk>
2025-12-03 01:21:46 +0000

    - Cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { cssModuleSnapshotSerializer } from './serializer/cssModuleSnapshotSerializer';
 import cssModuleTransformer from './transformer/cssModuleTransformer';
 
-// export * from './matchers';
-
 expect.addSnapshotSerializer(cssModuleSnapshotSerializer);
 
 export { cssModuleSnapshotSerializer, cssModuleTransformer };
